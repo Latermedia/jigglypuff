edd48fefd210cdc9f6e4a2d8be0865cf8d0b7483
imackinn <imackinn@alumni.uwaterloo.ca>
2018-01-14 18:20:05 -0800

    add dynamodb integration

--- jiggly_puff.rb
+++ jiggly_puff.rb
@@ -1,10 +1,18 @@
 require 'sinatra/base'
+require 'aws-sdk'
+
 
 class JigglyPuff < Sinatra::Base
+  dynamodb = Aws::DynamoDB::Client.new(region: 'us-east-1')
+
   get "/" do
-    "hello world"
+    @products = dynamodb.scan(table_name:ENV['TABLE_NAME']).items
+    erb :index
   end
 
+  not_found do
+    'This is nowhere to be found.'
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
